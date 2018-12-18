a8246a92a31102b8db8fa61d6bbd524567f54cc1
Chabane Yahi <yahichabane94@gmail.com>
2018-12-18 05:51:57 +0100

    modification de testServer

--- Test/Server/ServerTest.java
+++ Test/Server/ServerTest.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ../src/Server/Server.java;
+import ../../src/Server/Server.java;
 
 class ServerTest {
 
